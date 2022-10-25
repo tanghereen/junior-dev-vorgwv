959cff92511df4ee8b0daaa2c04c744e172483fd
tanghereen <93137545+tanghereen@users.noreply.github.com>
2022-10-25 19:07:41 -0400

    moved to src

--- to-do/src/index.js
+++ to-do/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,6 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// can register() and unregister() below to disable working offline mode
-serviceWorker.unregister();
